6287a49c6ab83be75c73f1874337c5e38bed9f25
Lars Juul <ljb@easv.dk>
2021-11-24 09:38:25 +0100

    Adding login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path: 'products', loadChildren: () =>
     import('./products/products.module')
-      .then(f => f.ProductsModule)}
+      .then(f => f.ProductsModule)},
+  {path: 'auth', loadChildren: () =>
+      import('./auth/auth.module')
+        .then(f => f.AuthModule)},
 ];
 
 @NgModule({
