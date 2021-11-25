23dbc011329f82ada75658c57c07a4feaad27ba9
Lars Juul <ljb@easv.dk>
2021-11-25 14:45:42 +0100

    Adding behavior subject for dynamic login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,31 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormBuilder} from '@angular/forms';
 import {AuthService} from '../shared/auth.service';
 import {LoginDto} from '../shared/login.dto';
+import {BehaviorSubject, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-inno-tech-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   loginForm = this.fb.group({
     username: [''],
     password: ['']
   });
-
+  private unsub: Subscription | undefined;
   constructor(private fb: FormBuilder,
               private _auth: AuthService) {
   }
 
   ngOnInit(): void {
+    this.unsub = this._auth.isLoggedIn$.subscribe(token => {
+      console.log('token', token);
+    })
+  }
+
+  ngOnDestroy(): void {
+    if(this.unsub) {
+      this.unsub.unsubscribe();
+    }
   }
 
   login() {
     const loginDto = this.loginForm.value as LoginDto;
     this._auth.login(loginDto)
       .subscribe(token => {
-        console.log('Token: ', token);
+        //console.log('Token: ', token);
       });
   }
+
+
 }
